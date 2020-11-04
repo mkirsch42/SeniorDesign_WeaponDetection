f046cca74db57d02d6e3bca6c6e955a3cf171e06
Mathew Kirschbaum <mathew.kirschbaum@uconn.edu>
2020-11-04 13:39:48 -0500

    Remove source and data from Docker build context.

--- src/main.py
+++ src/main.py
@@ -4,10 +4,8 @@ from cvlib.object_detection import (
 )
 import cv2
 
-image = cv2.imread('input/test.bmp')
+image = cv2.imread('input/flickr/1000366164.jpg')
+print('detecting objects...')
 detected_objects = detect_common_objects(image)
 image = draw_bbox(image, *detected_objects)
 
-cv2.imshow('YOLO', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
