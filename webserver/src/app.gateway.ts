3faa2fb4ed94b1646de4b772231b83f415717e1d
Mathew Kirschbaum <mathew.kirschbaum@uconn.edu>
2021-03-22 01:16:48 -0400

    Make websockets work on webserver.

--- webserver/src/app.gateway.ts
+++ webserver/src/app.gateway.ts
@@ -2,21 +2,22 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   OnGatewayInit,
-  WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
+import { DetectionDto } from "./detection-dto"
 
 @WebSocketGateway()
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private server: Server) {}
+  @WebSocketServer() private server: Server;
   private readonly logger: Logger = new Logger(AppGateway.name);
 
   afterInit(server: Server) {
-    this.logger.log('Init');
+    this.logger.log(this.server);
   }
 
   handleDisconnect(client: Socket) {
@@ -27,7 +28,7 @@ export class AppGateway
     this.logger.log(`Client connected: ${client.id}`);
   }
 
-  onDetection(payload: Object) {
-    this.server.emit(payload.toString());
+  onDetection(payload: DetectionDto) {
+    this.server.sockets.emit('image', payload);
   }
 }
---
+++ webserver/src/detection-dto.ts
@@ -0,0 +1,3 @@
+export class DetectionDto {
+  image: string;
+}
