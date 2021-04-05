2472db8bc87aa69f86479a477b8084b8e4b6d709
Swiftyjim <58717263+Swiftyjim@users.noreply.github.com>
2021-04-05 19:41:38 -0400

    Update Webserver (#1)
    * Update main.js
    
    * Up date Web look

--- webserver/static/main.js
+++ webserver/static/main.js
@@ -1,36 +1,20 @@
 const app = new Vue({
     el: '#app',
     data: {
-     title: 'Nestjs Websockets UI',
+     title: '',
      imageBytes: '',
      socket: null,
-     decoder: new TextDecoder(),
-     canvas: null,
-     canvasElem: null
+     decoder: new TextDecoder()
     },
     methods: {
      receivedMessage(message) {
         this.imageBytes = this.decoder.decode(new Uint8Array(message.image));
-        var img = new Image();
-        img.src = "data:image/jpeg;base64," + this.imageBytes;
-        img.onload = ()=>{
-            this.canvas.clearRect(0, 0, this.canvasElem.width, this.canvasElem.height)
-            this.canvas.drawImage(img, 0, 0)
-            message.boxes.forEach(box => {
-                this.canvas.beginPath();
-                this.canvas.rect(...box.rect);
-                this.canvas.strokeStyle = "red"
-                this.canvas.stroke();
-            });
-        }
     }
    },
-    mounted() {
-        this.socket = io('http://10.0.0.7')
-        this.socket.on('image', (message) => {
-            this.receivedMessage(message)
-        })
-        this.canvasElem = document.getElementById("myCanvas")
-        this.canvas = this.canvasElem.getContext("2d")
+    created() {
+     this.socket = io()
+     this.socket.on('image', (message) => {
+      this.receivedMessage(message)
+     })
     }
    })
